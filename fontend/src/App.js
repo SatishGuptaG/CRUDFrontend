a0117ee725a26e9890a2f5f27e993658364b2e7a
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2024-09-22 13:40:57 +0530

    add Brand List 80 percent
    add Brand List 80 percent

---
+++ fontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import BrandList from './component/BrandList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BrandList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
